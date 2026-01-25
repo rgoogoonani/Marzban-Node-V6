6af0090d9c645adf3286c41ff0b0a2ec477e2da3
rgoogoonani <68652911+rgoogoonani@users.noreply.github.com>
2026-01-25 15:17:51 +0330

    Change host from '::' to '0.0.0.0'

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
         logger.info(f"Node service running on :{SERVICE_PORT}")
         uvicorn.run(
             rest_service.app,
-            host="::",
+            host="0.0.0.0",
             port=SERVICE_PORT,
             ssl_keyfile=SSL_KEY_FILE,
             ssl_certfile=SSL_CERT_FILE,
